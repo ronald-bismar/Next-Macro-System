f21fbdfc53252f6fb16823e8012fe1557b7d0cce
Bismar <prueba@correo.com>
2023-12-02 20:30:58 -0400

    firs-commit next macro system

---
+++ js/index.js
@@ -0,0 +1,15 @@
+// JavaScript para agregar la clase "fade-in" a la imagen después de un retraso
+const fadeImage = document.getElementById("fade-image");
+setTimeout(() => {
+    fadeImage.classList.add("fade-in");
+}, 1000); // Ajusta el valor del retraso en milisegundos según desees
+
+
+
+const nav = document.querySelector('.navbar');
+
+window.addEventListener('scroll', function() {
+  const scrollY = window.scrollY;
+
+  nav.style.opacity = + scrollY / 500;
+});
