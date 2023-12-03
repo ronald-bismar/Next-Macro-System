01f96c7213497f68305ae781348c040b1f088d16
Bismar <prueba@correo.com>
2023-12-02 23:12:27 -0400

    Se arregló el efecto del fondo del navbar que no se cambiaba al hacer scroll hacia abajo

--- js/index.js
+++ js/index.js
@@ -1,15 +1,12 @@
 // JavaScript para agregar la clase "fade-in" a la imagen después de un retraso
 const fadeImage = document.getElementById("fade-image");
 setTimeout(() => {
-    fadeImage.classList.add("fade-in");
+  fadeImage.classList.add("fade-in");
 }, 1000); // Ajusta el valor del retraso en milisegundos según desees
 
+const nav = document.querySelector(".navbar");
 
-
-const nav = document.querySelector('.navbar');
-
-window.addEventListener('scroll', function() {
+window.addEventListener("scroll", function () {
   const scrollY = window.scrollY;
-
-  nav.style.opacity = + scrollY / 500;
+  nav.style.backgroundColor = "rgba(0, 67, 109," + scrollY / 500 + ")";
 });
